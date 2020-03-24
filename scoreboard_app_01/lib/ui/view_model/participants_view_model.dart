a12d9375bd51dab7e483a83c0e8d1edccbb1d53e
Grigori Hlopkov <grigori.hlopkov@gmail.com>
2020-03-24 19:42:48 +0100

    use usecases

--- scoreboard_app_01/lib/ui/view_model/participants_view_model.dart
+++ scoreboard_app_01/lib/ui/view_model/participants_view_model.dart
@@ -3,39 +3,46 @@ import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:scoreboardapp/data/repository/participants_repository_impl.dart';
 import 'package:scoreboardapp/domain/entity/participant.dart';
+import 'package:scoreboardapp/domain/use_case/create_participant_use_case.dart';
+import 'package:scoreboardapp/domain/use_case/delete_participant_use_case.dart';
+import 'package:scoreboardapp/domain/use_case/get_participants_use_case.dart';
+import 'package:scoreboardapp/domain/use_case/update_participant_use_case.dart';
 
 class ParticipantsViewModel extends ChangeNotifier {
-  final repository = ParticipantsRepositoryImpl();
+  final CreateParticipantUseCase _createParticipantUseCase = CreateParticipantUseCase();
+  final DeleteParticipantUseCase _deleteParticipantUseCase = DeleteParticipantUseCase();
+  final UpdateParticipantUseCase _updateParticipantUseCase = UpdateParticipantUseCase();
+  final GetParticipantsUseCase _getParticipantsUseCase = GetParticipantsUseCase();
 
   // Handle duplicates
 
   UnmodifiableListView<Participant> get participants =>
-      UnmodifiableListView(repository.getParticipants());
+      UnmodifiableListView(_getParticipantsUseCase());
 
   void create(Participant participant) {
-    repository.createParticipant(participant);
+    _createParticipantUseCase(participant);
     notifyListeners();
   }
 
   void update(Participant participant) {
-    repository.updateParticipant(participant);
+    _updateParticipantUseCase(participant);
     notifyListeners();
   }
 
   void delete(Participant participant) {
-    repository.deleteParticipant(participant);
+    _deleteParticipantUseCase(participant);
     notifyListeners();
   }
 
   void incrementScore(Participant participant) {
     participant.score++;
-    repository.updateParticipant(participant);
+    _updateParticipantUseCase(participant);
     notifyListeners();
   }
 
   void decrementScore(Participant participant) {
     participant.score--;
-    repository.updateParticipant(participant);
+    _updateParticipantUseCase(participant);
     notifyListeners();
   }
 }
