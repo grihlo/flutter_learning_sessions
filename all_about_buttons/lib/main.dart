e53c509d1e06ac1b49c37de8fdda9ce83828b848
Grigori Hlopkov <grigori.hlopkov@gmail.com>
2020-05-07 11:13:45 +0200

    add example material buttons

--- all_about_buttons/lib/main.dart
+++ all_about_buttons/lib/main.dart
@@ -6,32 +6,76 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'All about buttons 📱',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PageView(
+        children: <Widget>[
+          MaterialButtonsBasicsPage(title: 'Material buttons'),
+          MaterialButtonsExtendedPage(title: 'Material button drop down',),
+          MaterialButtonsAdvancedPage(title: 'Material buttons'),
+        ],
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MaterialButtonsBasicsPage extends StatelessWidget {
+  MaterialButtonsBasicsPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              onPressed: () {},
+              child: Text("FlatButton"),
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text("RaisedButton"),
+            ),
+            OutlineButton(
+              onPressed: () {},
+              child: Text("OutlineButton"),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.send),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print("wow!"),
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MaterialButtonsExtendedPage extends StatefulWidget {
+  MaterialButtonsExtendedPage({Key key, this.title}) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final String title;
+
+  @override
+  _MaterialButtonsExtendedPageState createState() => _MaterialButtonsExtendedPageState();
+}
+
+class _MaterialButtonsExtendedPageState extends State<MaterialButtonsExtendedPage> {
+  String dropdownValue = 'DropdownButton1';
+  String popMenuValue = 'PopupMenuButton1';
 
   @override
   Widget build(BuildContext context) {
@@ -43,20 +87,79 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            DropdownButton<String>(
+              value: dropdownValue,
+              onChanged: (String newValue) {
+                setState(() {
+                  dropdownValue = newValue;
+                });
+              },
+              items: <String>['DropdownButton1', 'DropdownButton2']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            PopupMenuButton<String>(
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  value: "PopupMenuButton1",
+                  child: Text("PopupMenuButton1"),
+                ),
+                PopupMenuItem(
+                  value: "PopupMenuButton2",
+                  child: Text("PopupMenuButton2"),
+                ),
+              ],
+              initialValue: popMenuValue,
+              onSelected: (value) {
+                print("value:$value");
+              },
+              icon: Icon(Icons.menu),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+    );
+  }
+}
+
+class MaterialButtonsAdvancedPage extends StatelessWidget {
+  MaterialButtonsAdvancedPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Semantics
+            InkWell(
+              onTap: () {},
+              child: Text("InkWell"),
+            ),
+            InkResponse(
+              onTap: () {},
+              child: Text("InkResponse"),
+            ),
+            RawMaterialButton(
+              onPressed: () {},
+              child: Text("RawMaterialButton"),
+            ),
+            MaterialButton(
+              onPressed: () {},
+              child: Text("MaterialButton"),
+            ),
+          ],
+        ),
       ),
     );
   }
